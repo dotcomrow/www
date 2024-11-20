ffe1b068e7b856d9c56d65a1ff7f2e1ff21edb6b
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-11-20 16:49:12 -0500

    adding projects page
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ src/components/ui/project-cards/SnapspotCard.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Card, CardBody, Image, Button, Slider } from "@nextui-org/react";
+import {Link} from "@nextui-org/link";
+
+export default function SnapspotCard() {
+
+  return (
+    <Card
+      isBlurred
+      className="border-none bg-background/60 dark:bg-default-100/50"
+      shadow="sm"
+    >
+      <CardBody>
+        <div className="grid grid-cols-4 gap-6 items-center justify-center">
+          <div className="relative col-span-2 md:col-span-1">
+            <Image
+              alt="Album cover"
+              className="object-cover"
+              height={150}
+              width="100%"
+              shadow="md"
+              src="/assets/images/project-cards/snapspot.png"
+            />
+          </div>
+
+          <div className="flex flex-col col-span-2 md:col-span-3">
+            <div className="flex justify-between items-start">
+              <div className="flex flex-col gap-0">
+                <h1 className="text-large font-medium mt-2">Snapspot (Unfinished)</h1>
+                <p className="text-small text-foreground/80">Snapspot is a project to allow users to request a picture of any location.  This goal of this project was mainly to learn NextJs and supporting technologies.</p>
+              </div>
+            </div>
+
+            <div className="flex absolute bottom-0">
+              <Link href="https://snapspot.suncoast.systems">Visit Snapspot</Link>
+            </div>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
