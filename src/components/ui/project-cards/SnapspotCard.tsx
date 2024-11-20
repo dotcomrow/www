438c318ae24d1c9211c6d25cc74ab895da2e4704
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-11-20 17:03:08 -0500

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/components/ui/project-cards/SnapspotCard.tsx
+++ src/components/ui/project-cards/SnapspotCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Card, CardBody, Image, Button, Slider } from "@nextui-org/react";
+import { Card, CardBody, Image } from "@nextui-org/react";
 import {Link} from "@nextui-org/link";
 
 export default function SnapspotCard() {
@@ -12,7 +12,7 @@ export default function SnapspotCard() {
       shadow="sm"
     >
       <CardBody>
-        <div className="grid grid-cols-4 gap-6 items-center justify-center">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6 items-center justify-center">
           <div className="relative col-span-2 md:col-span-1">
             <Image
               alt="Album cover"
@@ -25,7 +25,7 @@ export default function SnapspotCard() {
           </div>
 
           <div className="flex flex-col col-span-2 md:col-span-3">
-            <div className="flex justify-between items-start">
+            <div className="flex justify-between items-start mb-4">
               <div className="flex flex-col gap-0">
                 <h1 className="text-large font-medium mt-2">Snapspot (Unfinished)</h1>
                 <p className="text-small text-foreground/80">Snapspot is a project to allow users to request a picture of any location.  This goal of this project was mainly to learn NextJs and supporting technologies.</p>
